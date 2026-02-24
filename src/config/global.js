2ef533f10cb10883089c5635ebe17bfde0afa784
PABG1990 <pedro19908454@gmail.com>
2026-02-24 16:46:27 -0500

    Add_cfa

--- src/config/global.js
+++ src/config/global.js
@@ -124,7 +124,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF1_73310057_DU.pdf',
       },
       {
         icono: 'fas fa-download',
@@ -379,7 +379,7 @@ export default {
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: 'Laura Paola Gelvez Manosalva',
+          nombre: 'Sandra Liliana Cristancho Cruz',
           cargo: 'Evaluadora para contenidos inclusivos y accesibles',
           centro: 'Centro Agroturístico - Regional Santander',
         },
